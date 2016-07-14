44523c7952456a4e84086957c8920abb147c6896
Adam Onishi <onishiweb@gmail.com>
2016-07-14 10:36:36 +0100

    Add new main js and scss files

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// Require module
+// require('o-header');
+
+// Wait until the page has loaded
+if (document.readyState === 'interactive' || document.readyState === 'complete') {
+	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+}
+document.addEventListener('DOMContentLoaded', function() {
+	// Dispatch a custom event that will tell all required modules to initialise
+	document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
+});
