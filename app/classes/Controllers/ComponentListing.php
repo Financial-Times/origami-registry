03771d2515026e1ab502aaba336c6f08850e30d2
Adam Onishi <onishiweb@gmail.com>
2016-07-27 10:54:45 +0100

    Add catch all category for untagged modules

--- app/classes/Controllers/ComponentListing.php
+++ app/classes/Controllers/ComponentListing.php
@@ -31,16 +31,25 @@ class ComponentListing extends BaseController {
 					'title' => 'Utilities',
 					'modules' => array(),
 				),
+				'other' => array(
+					'title' => 'Misc',
+					'modules' => array(),
+				),
 			);
 
 		foreach (Component::findAll('c.is_origami IS TRUE') as $component) {
+			$cat = $component->origami_category;
+			if ($cat === '') {
+				$cat = 'other';
+			}
+
 			if ($component->latest_stable_version) {
-				$viewdata[$component->origami_category]['modules'][] = array_merge(
+				$viewdata[$cat]['modules'][] = array_merge(
 					$component->toArray(),
 					$component->latest_stable_version->toArray()
 				);
 			} elseif($component->latest_version) {
-				$viewdata[$component->origami_category]['modules'][] = array_merge(
+				$viewdata[$cat]['modules'][] = array_merge(
 					$component->toArray(),
 					$component->latest_version->toArray()
 				);
