f3e9213dda2060d34a139dd1fb636d89c48bc0c6
Adam Onishi <onishiweb@gmail.com>
2016-07-27 10:44:24 +0100

    Rename group to category

--- app/classes/Controllers/ComponentListing.php
+++ app/classes/Controllers/ComponentListing.php
@@ -35,12 +35,12 @@ class ComponentListing extends BaseController {
 
 		foreach (Component::findAll('c.is_origami IS TRUE') as $component) {
 			if ($component->latest_stable_version) {
-				$viewdata[$component->origami_group]['modules'][] = array_merge(
+				$viewdata[$component->origami_category]['modules'][] = array_merge(
 					$component->toArray(),
 					$component->latest_stable_version->toArray()
 				);
 			} elseif($component->latest_version) {
-				$viewdata[$component->origami_group]['modules'][] = array_merge(
+				$viewdata[$component->origami_category]['modules'][] = array_merge(
 					$component->toArray(),
 					$component->latest_version->toArray()
 				);
--- app/classes/Origami/ComponentVersion.php
+++ app/classes/Origami/ComponentVersion.php
@@ -17,7 +17,7 @@ final class ComponentVersion extends Model {
 
 	private $component;
 
-	protected $fields = array('id', 'component_id', 'tag_name', 'datetime_last_cached', 'datetime_created', 'is_valid', 'description', 'origami_type', 'origami_group', 'origami_version', 'support', 'service_url', 'readme_gfm', 'support_status', 'ci_status', 'has_js', 'has_css', 'bundlesize_js', 'bundlesize_css');
+	protected $fields = array('id', 'component_id', 'tag_name', 'datetime_last_cached', 'datetime_created', 'is_valid', 'description', 'origami_type', 'origami_category', 'origami_version', 'support', 'service_url', 'readme_gfm', 'support_status', 'ci_status', 'has_js', 'has_css', 'bundlesize_js', 'bundlesize_css');
 	protected $datefields = array('datetime_last_cached', 'datetime_created');
 
 
@@ -213,7 +213,7 @@ final class ComponentVersion extends Model {
 				foreach (array(
 					'description' => 'description',
 					'origamiType' => 'origami_type',
-					'origamiGroup' => 'origami_group',
+					'origamiCategory' => 'origami_category',
 					'origamiVersion' => 'origami_version',
 					'support' => 'support',
 					'supportStatus' => 'support_status',
