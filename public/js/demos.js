bb6e237b30c98720b712887307647ca9a527e347
Adam Onishi <onishiweb@gmail.com>
2016-08-23 16:03:20 +0100

    Update demo styles

--- public/js/demos.js
+++ public/js/demos.js
@@ -3,32 +3,14 @@
 'use strict';
 
 $(function() {
-	$('.demo-embed-link').on('click', function(e) {
-		e.preventDefault();
-	});
-
-	$('.demo-selector input').on('change', function() {
-		var demo = $('#demo-'+this.value);
-		demo[(this.checked?'add':'remove')+'Class']('visible');
-		if (demo.hasClass('visible')) {
-			var ifr = demo.find('.demo-frame').get(0);
-			ifr.src = ifr.src;
-		}
-	});
-	$('#transparency-toggle').on('change', function() {
-		$('.demo-frame')[(this.checked?'add':'remove')+'Class']('transparency-checkerboard');
-	});
-	$('#code-toggle').on('change', function() {
-		$('.demo-container').toggleClass('with-source');
-	});
-	$('.demo-source').each(function(i, source) {
+	$('.demo__source').each(function(i, source) {
 		var $source = $(source);
 		$.getJSON($source.attr('data-src'), function(resp) {
 			if (resp.code) {
 				var codeEl = $('<code class="html"></code>').text(resp.code).addClass('js-auto-select-target');
-				$source.append(codeEl).prepend('<button class="auto-select-action js-auto-select-action">Select full code snippet</button>');
+				$source.append(codeEl).prepend('<button class="demo__auto-select-action js-auto-select-action">Select full code snippet</button>');
 			} else if (resp.errors) {
-				var html = "<div class='demo-source demo-source--errors'><p>There are problems with this demo:</p><ul>";
+				var html = "<div class='demo__source demo__source--errors'><p>There are problems with this demo:</p><ul>";
 				resp.errors.forEach(function(error) {
 					html += '<li><strong>Line '+error.line+':</strong> '+error.message+'</li>';
 				});
@@ -37,15 +19,11 @@ $(function() {
 			}
 		});
 	});
-});
 
-$(function() {
 	$('iframe').on('load', function() {
-		$(this).closest('.demo-wrapper').find('.activity').removeClass('activity');
+		$(this).closest('.demo').find('.activity').removeClass('activity');
 	});
-});
 
-$(function() {
 	$('.link-refresh').click(function(e) {
 		var btn = $(this);
 		btn.addClass('activity');
@@ -60,6 +38,7 @@ $(function() {
 			}
 		});
 	});
+
 	$('.version-select').on('change', function() {
 		if ($(this.selectedOptions[0]).hasClass('option-refresh')) {
 			$(this).attr('disabled', 'disabled').parent().addClass('activity');
