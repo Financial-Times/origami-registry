66eb07d71b6afce8091790ada8f0dea0ffa6492b
Adam Onishi <onishiweb@gmail.com>
2016-09-28 12:02:27 +0100

    Turn sticky-sidebar into a module

--- public/js/sticky-sidebar.js
+++ public/js/sticky-sidebar.js
@@ -2,37 +2,40 @@
 
 'use strict';
 
-var oViewport = require('o-viewport');
+module.exports = function() {
+	var oViewport = require('o-viewport');
 
-$(function() {
-	var stickySidebar = $('.js-sticky-sidebar'),
-		stickyTop = 0;
+	$(function() {
+		var stickySidebar = $('.js-sticky-sidebar'),
+			stickyTop = 0;
 
-	function updateStickyElement() {
-		var scrollTop = oViewport.getScrollPosition().top;
+		function updateStickyElement() {
+			var scrollTop = oViewport.getScrollPosition().top;
 
-		if (scrollTop > stickyTop) {
-			$(stickySidebar).addClass('js-sticky-sidebar--active');
-		} else {
-			$(stickySidebar).removeClass('js-sticky-sidebar--active');
+			if (scrollTop > stickyTop) {
+				$(stickySidebar).addClass('js-sticky-sidebar--active');
+			} else {
+				$(stickySidebar).removeClass('js-sticky-sidebar--active');
+			}
 		}
-	}
 
-	function initStickyElement() {
-		stickyTop = $(stickySidebar).offset().top;
+		function initStickyElement() {
+			stickyTop = $(stickySidebar).offset().top;
 
-		if (window.matchMedia("(min-width: 900px)").matches) {
-			oViewport.listenTo('scroll');
-			document.body.addEventListener('oViewport.scroll', updateStickyElement, false);
-		} else {
-			document.body.removeEventListener('oViewport.scroll', updateStickyElement, false);
+			if (window.matchMedia("(min-width: 900px)").matches) {
+				oViewport.listenTo('scroll');
+				document.body.addEventListener('oViewport.scroll', updateStickyElement, false);
+			} else {
+				document.body.removeEventListener('oViewport.scroll', updateStickyElement, false);
+			}
 		}
-	}
 
-	if ($(stickySidebar).length) {
-		oViewport.listenTo(['scroll', 'resize']);
+		if ($(stickySidebar).length) {
+			oViewport.listenTo(['scroll', 'resize']);
 
-		initStickyElement();
-		document.body.addEventListener('oViewport.resize', initStickyElement, false);
-	}
-});
+			initStickyElement();
+			document.body.addEventListener('oViewport.resize', initStickyElement, false);
+		}
+	});
+
+}
